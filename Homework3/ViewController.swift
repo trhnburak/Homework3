c39b45789ef365a994afabc0b63319d6ba5f9133
BurakTurhan <trhnburak@gmail.com>
2023-01-25 21:22:29 +0300

    Initial Commit

---
+++ Homework3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework3
+//
+//  Created by Burak Turhan on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
